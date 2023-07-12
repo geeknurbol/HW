cc16293c7562e3ad3440155a28f4f536ac6c8341
Nurbol <nurbogeek.1@gmail.com>
2023-07-12 14:41:15 +0600

    hw

---
+++ js/home_works.js
@@ -0,0 +1,54 @@
+const gmailInput = document.querySelector('#gmail_input')
+const gmailButton = document.querySelector('#gmail_button')
+const gmailResult = document.querySelector('#gmail_result')
+
+const regExp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+gmailButton.onclick = () => {
+    if (regExp.test(gmailInput.value)) {
+        gmailResult.innerHTML = 'YOUR EMAIL ADDRESS IS VALID!'
+        gmailResult.style.color = 'green'
+    } else {
+        gmailResult.innerHTML = 'YOUR EMAIL ADDRESS IS NOT VALID'
+        gmailResult.style.color = 'red'
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+const childBlock = document.querySelector('.child_block')
+
+let positionX = 0
+let positionY = 0
+
+const move = () => {
+    if (positionX < 449 && positionY === 0) {
+        positionX+=2
+        childBlock.style.left = `${positionX}px`
+        setTimeout(move, 10)
+    } else if (positionX >= 449 && positionY < 449) {
+        positionY+=2
+        childBlock.style.top = `${positionY}px`
+        setTimeout(move, 10)
+    } else if (positionX > 0 && positionY > 0) {
+        positionX-=2
+        childBlock.style.left = `${positionX}px`
+        setTimeout(move, 10)
+    } else if (positionX === 0 && positionY > 0) {
+        positionY-=2
+        childBlock.style.top = `${positionY}px`
+        setTimeout(move, 10)
+    }
+}
+
+move()
